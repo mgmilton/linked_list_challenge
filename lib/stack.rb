4dd25b0d5d9778a0ada59dcb5fe6a6fc72a51122
mgmilton <matt.milton@gmail.com>
2018-05-16 10:25:27 -0600

    adds pop method to stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -10,10 +10,16 @@ class Stack
   def empty?
     if list.count == 0
       return true
+    else
+      return false
     end
   end
 
   def push(num)
     list.append(num)
+  end
+
+  def pop
+    list.pop
   end
 end
